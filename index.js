5991358c75e324714bacad4a3e3dd071ad72ddf1
fojndnfjdjn <119886430+fojndnfjdjn@users.noreply.github.com>
2022-12-09 01:55:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const { SheetDatabase } = require('sheets-database');
+const fs = require('fs');
 
 const db = new SheetDatabase(process.env._ID);
 
@@ -28,7 +29,7 @@ const getData = async () => {
       'email_address',
     ]);
 
-    for (let index = 0; index < 999999999; index++) {
+    for (let index = 0; index < 500; index++) {
       const site_url = `${site_}${uniqueId}#`;
       const page = await browser.newPage();
       await page.setDefaultNavigationTimeout(0);
@@ -68,8 +69,14 @@ const getData = async () => {
       await page.close();
       uniqueId = uniqueId + 1;
     }
-
     await browser.close();
+
+    uniqueId = uniqueId - 3;
+    fs.writeFile('uuid.txt', '' + uniqueId, { flag: 'wx' }, function (err) {
+      if (err) throw err;
+      console.log("It's saved!");
+    });
+
     return true;
   } catch (error) {
     console.error(`getData error: ${error}`);
