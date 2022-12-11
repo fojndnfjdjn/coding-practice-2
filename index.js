a95f7545c2a6b7721583b6ed67bbe4cf6df9678b
fojndnfjdjn <119886430+fojndnfjdjn@users.noreply.github.com>
2022-12-11 15:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const getData = async () => {
       'email_address',
     ]);
 
-    for (let index = 0; index < 700; index++) {
+    for (let index = 0; index < 200; index++) {
       const site_url = `${site_}${uniqueId}#`;
       const page = await browser.newPage();
       await page.setDefaultNavigationTimeout(0);
